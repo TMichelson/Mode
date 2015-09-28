79b3c971eb1d43e4d598585cc752e2247a633637
TMichelson <TMichelson27@gmail.com>
2015-09-28 12:21:51 -0400

    Update test_modeEstimate.R

--- test/test_modeEstimate.R
+++ test/test_modeEstimate.R
@@ -1,6 +1,6 @@
 test_modeEstimate <- function() {
   x <- c(3.2, 7.1, 4.3, 9.9, 2.9, 2.7, 5.6, 8.8, 3.3, 2.5,7.5,9.9,10.0)
   tst <- modeEstimate(x)
-  checkEqualsNumeric(tst, 3.290578)
+  checkEqualsNumeric(tst, 3.367459)
 
 }
